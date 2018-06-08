0d809e5aaee486030548c782c290d3d54b4933ca
Melinh Mai <melinh.mai@gmail.com>
2018-06-08 10:39:47 -0500

    api calls

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,8 +10,6 @@ function renderButtons(){
 
  }
 
- // ========================================================
-
  // This function handles events where one button is clicked
  $('#addGif').on('click', function(){
 
@@ -27,4 +25,24 @@ function renderButtons(){
  // This calls the renderButtons() function
  renderButtons();
     
-  
+
+ //calling API when button clicked
+
+ $('button').on('click',function(){
+    var x = $(this).data("search");
+   
+
+    var queryUrl  = `https://api.giphy.com/v1/gifs/search?q=`+topics+`&api_key=djALz2kux6YRp13Gfq1icR5DPV7gnwxe&limit=10`;
+    console.log(queryUrl);
+
+    $.ajax({url:queryUrl,method:'GET'})
+    .done(function(response){
+        console.log(response);
+        for(var i=0; i<response.data.length;i++){
+        $('#picView').prepend("<p>Rating: "+response.data[i].rating+"</p>");
+        $('#picView').prepend("<img src='"+response.data[i].images.downsized.url+"'>");
+
+        }
+       
+    })
+})
